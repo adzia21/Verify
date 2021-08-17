6603d3317aa7a463435de38ff2dc942e467f0de8
Angelika <angelika.swiackaa@gmail.com>
2021-08-17 11:43:55 +0200

    Quick fixes and copy button

--- src/main/java/com/verify/gui/CheckMailGUI.java
+++ src/main/java/com/verify/gui/CheckMailGUI.java
@@ -14,7 +14,7 @@ import com.verify.service.SMTPLookup;
 import java.util.ArrayList;
 import java.util.List;
 
-@Route("mail")
+@Route("mail-old")
 public class CheckMailGUI extends VerticalLayout  {
     SMTPLookup smtpLookup = new SMTPLookup();
 
--- src/main/java/com/verify/gui/MultiThreadMailGUI.java
+++ src/main/java/com/verify/gui/MultiThreadMailGUI.java
@@ -1,10 +1,13 @@
 package com.verify.gui;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.listbox.MultiSelectListBox;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.verify.service.SMTPLookupMultiThread;
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Route("mail")
+@JsModule("./copytoclipboard.js")
 public class MultiThreadMailGUI extends VerticalLayout  {
     SMTPLookupMultiThread smtpmxLookup = new SMTPLookupMultiThread();
     public MultiThreadMailGUI() {
@@ -20,17 +24,18 @@ public class MultiThreadMailGUI extends VerticalLayout  {
         checker.setMaxWidth("500px");
 
         TextField nameField = new TextField();
-        nameField.setLabel("Imię");
+        nameField.setLabel("Name");
         nameField.setRequiredIndicatorVisible(true);
 
         TextField surnameField = new TextField();
-        surnameField.setLabel("Nazwisko");
+        surnameField.setLabel("Surname");
 
         TextField domainField = new TextField();
-        domainField.setLabel("Domena");
+        domainField.setLabel("Domain");
         domainField.setRequiredIndicatorVisible(true);
 
-        Button search = new Button("Szukaj");
+        Button search = new Button("Search");
+        Button copy = new Button("Copy");
 
         HorizontalLayout actions = new HorizontalLayout();
         actions.add(search);
@@ -58,7 +63,15 @@ public class MultiThreadMailGUI extends VerticalLayout  {
             listBox.setItems(result);
             listBox.select(checked);
 
+            TextArea textArea = new TextArea();
+            textArea.setValue(checked.toString());
+
+            copy.addClickListener(
+                    e -> UI.getCurrent().getPage().executeJs("window.copyToClipboard($0)", textArea.getValue())
+            );
+
             add(listBox);
+            add(copy);
         });
 
         add(checker);
